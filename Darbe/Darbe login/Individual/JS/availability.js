0aaf8c08f3302e65616b4e948d92b7bb0ae95681
Mario Diosdado <diosdado.mario@gmail.com>
2021-06-09 14:05:42 -0500

    Added hours through loop on js file

--- Darbe/Darbe login/Individual/JS/availability.js
+++ Darbe/Darbe login/Individual/JS/availability.js
@@ -1,36 +1,50 @@
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+const hoursAM = ['12:00am'];
+const hoursPM = ['12:00pm'];
+for (n=1;n<12;n++){
+    hoursAM.push(n + ":00am");
+    hoursPM.push(n + ":00pm");   
+}
 
 for (i=0;i<days.length;i++) {
     $('.formStyleB').append(`
     <div class="form-row align-items-center">
     <div class="col-4 my-1">
         <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="monday">
+            <input class="form-check-input" type="checkbox" value="" id="${days[i]}">
             <label class="form-check-label" for="defaultCheck1">
               ${days[i]}
             </label>
           </div>
     </div>
     <div class="col-auto my-1">
-        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-            <option selected>Choose...</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+        <select class="custom-select am${i} mr-sm-2" id="inlineFormCustomSelect">
         </select>
     </div>
     <div class="col-1 transition">
         to
     </div>
     <div class="col-auto my-1">
-        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-            <option selected>Choose...</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+        <select class="custom-select pm${i}  mr-sm-2" id="inlineFormCustomSelect">
         </select>
     </div>
 </div>
 <br>
     `)
 }
+
+time();
+
+function time() {
+    for(y=0;y<days.length;y++){
+        for(j=0;j<12;j++){
+            $('.am' + y).append(`
+            <option value="${j}">${hoursAM[j]}</option>
+            `)
+            $('.pm' + y).append(`
+            <option value="${j}">${hoursPM[j]}</option>
+            `)
+        }
+    }
+}
+
